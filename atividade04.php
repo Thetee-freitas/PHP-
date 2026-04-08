6588e050cdc1a1ce4e1d2e5bca09c751e8164b84
Thetee-freitas <stephanny.freitas1234@gmail.com>
2026-04-08 09:46:54 -0300

    Atividade06

--- atividade04.php
+++ atividade04.php
@@ -1,7 +1,8 @@
-Faça um código PHP que utiliza apenas a função echo para 
-exibir a soma dos números ímpares de 1 a 20. <br>
+<?php
 
-<?php 
+//Faça um código PHP que utiliza apenas a função echo para 
+//exibir a soma dos números ímpares de 1 a 20.
+
 $impares = 1+3+5+7+9+11+13+15+17+19;
 echo "O resultado da soma dos números ímpares é: $impares";
 
